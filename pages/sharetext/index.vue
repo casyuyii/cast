e93950229e28c696a2c09d19380e73431be636e7
Cas <153147743+CasYuY@users.noreply.github.com>
2024-03-13 23:45:01 +0800

    feat: share tips

--- pages/sharetext/index.vue
+++ pages/sharetext/index.vue
@@ -12,6 +12,12 @@
     </div>
 
     <Button label="Share" :loading="shareLoding" @click="shareText" class="" />
+
+    <transition-group name="p-message">
+      <Message v-for="msg of messages" :key="msg.id" :severity="msg.severity">{{
+        msg.content
+      }}</Message>
+    </transition-group>
   </div>
 </template>
 
@@ -22,10 +28,21 @@ import { ShareText } from "~/api/share-text"
 const textValue = ref("")
 const shareLoding = ref(false)
 const secretKey = ref("")
+const sharedText = ref("")
+const messages = ref([])
+let count = ref(0)
 
 const shareText = async () => {
   shareLoding.value = true
-  await ShareText({ text: textValue.value, secretKey: secretKey.value })
+  sharedText.value = await ShareText({
+    text: textValue.value,
+    secretKey: secretKey.value,
+  })
+
+  messages.value = [
+    { severity: "success", content: "Text Shared", id: count.value++ },
+  ]
+
   shareLoding.value = false
 }
 </script>
