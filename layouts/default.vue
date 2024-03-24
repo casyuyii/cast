ea8842b9e218c4f08a45aa3c04d27d759b72b135
Cas <153147743+CasYuY@users.noreply.github.com>
2024-03-24 17:00:58 +0800

    reset: delete tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Menubar :model="items"> </Menubar>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+  },
+  {
+    label: "Features",
+    icon: "pi pi-star",
+  },
+
+  {
+    label: "Contact",
+    icon: "pi pi-envelope",
+    badge: 3,
+  },
+])
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,7 @@
-import path from "path";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
   modules: ["nuxt-primevue"],
-  primevue: {
-    options: {
-      unstyled: true,
-    },
-    importPT: { from: path.resolve(__dirname, "./presets/lara/") },
-  },
-  css: ["~/assets/css/tailwind.css"],
-});
+  primevue: {},
+  css: ["primevue/resources/themes/aura-light-green/theme.css"],
+})
