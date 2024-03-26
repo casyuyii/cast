3801f0862b9a31a793ce5543cdce3abad0392d5e
Cas <153147743+CasYuY@users.noreply.github.com>
2024-03-26 22:41:48 +0800

    feat: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-primevue", "@nuxtjs/eslint-module"],
+  modules: ["nuxt-primevue", "@nuxtjs/eslint-module", "@nuxtjs/tailwindcss"],
   primevue: {},
   css: ["primevue/resources/themes/aura-light-green/theme.css"],
 })
