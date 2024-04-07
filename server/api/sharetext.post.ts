64a44d0f95ddac9a2bf7feffb25ed00bbd3f0168
Cas <153147743+CasYuY@users.noreply.github.com>
2024-04-08 01:23:28 +0800

    fix: env :(

--- server/api/sharetext.post.ts
+++ server/api/sharetext.post.ts
@@ -1,6 +1,5 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-  console.log(`get post ${body}`)
 
   await useStorage("redis").setItem("t1", body.text ?? "test string")
 
