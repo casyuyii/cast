99cf58df6713c8fd1b6bbf42c8f5b721eaa7452a
Cas <153147743+CasYuY@users.noreply.github.com>
2025-08-25 23:16:07 +0800

    feat: about me link

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-72 items-center justify-center">
-      <h1 className="text-2xl">Home page, click on the navigation menu to get started</h1>
+      <h1 className="text-2xl">This is a personal project</h1>
     </div>
   );
 }
--- packages/client/components/custom/navigation.tsx
+++ packages/client/components/custom/navigation.tsx
@@ -17,25 +17,31 @@ export default function NavigationMenuDemo() {
         <Button variant="ghost">Home</Button>
       </Link>
 
-      <NavigationMenu viewport={false} className="z-50">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Text</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-4">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link href="/text/share">Share</Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link href="/text/get">Get</Link>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div className="flex items-center gap-4">
+        <NavigationMenu viewport={false} className="z-50">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Text</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid gap-4">
+                  <li>
+                    <NavigationMenuLink asChild>
+                      <Link href="/text/share">Share</Link>
+                    </NavigationMenuLink>
+                    <NavigationMenuLink asChild>
+                      <Link href="/text/get">Get</Link>
+                    </NavigationMenuLink>
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <Link href="https://www.notion.so/Notes-table-304e00d84228430f9b72e765ddec0129">
+          <Button variant="link">About Me</Button>
+        </Link>
+      </div>
     </div>
   );
 }
