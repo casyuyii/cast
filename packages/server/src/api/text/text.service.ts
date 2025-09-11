af42b2e852e5f5f9fb68e6712c461473694f349a
Cas <153147743+casyuyii@users.noreply.github.com>
2025-09-11 22:36:32 +0800

    feat: text timeout(default 300 seconds)

--- packages/server/src/api/text/text.service.ts
+++ packages/server/src/api/text/text.service.ts
@@ -24,7 +24,14 @@ export class TextService {
   async shareText(text: string) {
     const db = this.mongoService.getTextDb();
     const collection = db.collection('texts');
-    const result = await collection.insertOne({ text });
+    await collection.createIndex({ expireAt: 1 }, { expireAfterSeconds: 300 });
+    const result = await collection.insertOne({
+      text,
+      expireAt: new Date(),
+    });
+    if (!result.acknowledged) {
+      return { success: false, text: 'Failed to share text' };
+    }
     return { success: true, code: result.insertedId.toString() };
   }
 }
