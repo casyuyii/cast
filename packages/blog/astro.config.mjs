ecaeb17addef5d6764714a56330bc4a2e0638236
Cas <153147743+casyuyii@users.noreply.github.com>
2025-10-14 21:03:03 +0800

    feat: resume redirect

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/resume": "https://docs.google.com/document/d/1LThX44LmBYZfaO6oM9SfN2nhyDP4g3ODcG_feOuYSNM",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
